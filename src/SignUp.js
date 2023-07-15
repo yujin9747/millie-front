4ea5cf834357a5297ca7dae83945f2fa9bb47c32
jang-yujin <jyj9747@naver.com>
2023-07-15 16:55:05 +0900

    :bug: [SignUp]: nickname form data 가져오지 못하는 형상 디버깅
    .

--- src/SignUp.js
+++ src/SignUp.js
@@ -44,7 +44,7 @@ function SignUp() {
 
     const onSignUp = async (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget[0]);
+        const data = new FormData(event.currentTarget);
         console.log({
             nickname: data.get('nickname'),
         });
